e0bcd8c28f9ba92930a706122fc490799d79f93c
maruseva <maruseva@yandex.ru>
2022-06-13 22:09:29 +0300

    add js

--- final _project/js/script.js
+++ final _project/js/script.js
@@ -5,11 +5,18 @@ function GoUrl(url){
 var menu = document.getElementById('menu');
 var addMenu = document.getElementById('addmenu');
 addMenu.addEventListener('click', function(){
-    menu.style.visibility = 'visible';
-    menu.style.width = '100vw';
+    menu.style.left = '0';
+    menu.style.opacity = '1';
 });
 var close = document.getElementById('close');
 close.addEventListener('click', function(){
-    menu.style.visibility = 'hidden';
-    menu.style.width = '0';
-})
+    menu.style.left = '-100vw';
+    menu.style.opacity = '0';
+});
+
+var back = document.getElementById('back');
+if (back !== null) {
+    back.addEventListener('click', function(){
+    window.history.back(); return false;
+    });
+} 
